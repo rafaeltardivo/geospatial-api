53582900d8d290d2625527d3d7e2f6c4fece8c20
rafaeltardivo <rafaeldt.br@gmail.com>
2022-05-11 20:06:55 -0300

    Update ATM view docstring

--- app/api/views.py
+++ app/api/views.py
@@ -1,9 +1,11 @@
-from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView
 
 from .models import ATM
 from .serializers import ATMSerializer
 
 
-class ATMView(generics.ListCreateAPIView):
+class ATMView(ListCreateAPIView):
+    """ATM List/Create view."""
+
     queryset = ATM.objects.all()
     serializer_class = ATMSerializer
